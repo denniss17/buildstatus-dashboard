49ca865f631ed8700eb4e6881304fa9861da519e
Dennis Schroer <dev@dennisschroer.nl>
2017-04-30 17:15:41 +0200

    Add random status service

---
+++ src/services/statuses-random/statuses-random.class.js
@@ -0,0 +1,29 @@
+const logger = require('winston');
+const Status = require('../../models/status');
+
+const statusResults = [Status.StatusResult.SUCCESS, Status.StatusResult.FAILURE, Status.StatusResult.RUNNING, Status.StatusResult.UNKNOWN];
+
+class RandomStatusesService {
+  constructor(options){
+    this.options = options || {};
+  }
+
+  get(issueKey) {
+    return Promise.resolve(this.generateRandomStatus(issueKey));
+  }
+
+  generateRandomStatus(issueKey) {
+    logger.info('Generating a random status for issue', issueKey);
+    return new Status({
+      issueKey,
+      result: statusResults[Math.floor(Math.random() * statusResults.length)],
+      origin: 'random'
+    })
+  }
+}
+
+module.exports = function(options) {
+  return new RandomStatusesService(options);
+};
+
+module.exports.Service = RandomStatusesService;
