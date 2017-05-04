8a40bc0ab0e9791f76acd8b46d74158bdb104ea8
Dennis Schroer <dennis.schroer@topicus.nl>
2017-05-04 08:49:32 +0200

    Improve the layout of an issue card: show two lines of description and remove footer

--- ui/src/issues/IssueList.js
+++ ui/src/issues/IssueList.js
@@ -8,14 +8,11 @@ class IssueList extends Component {
 
   renderIssue(issue) {
     return (
-      <div className="col-2" key={issue.key}>
+      <div className="col-lg-2 col-md-3 col-sm-6 col-12" key={issue.key}>
         <div className={"card card-inverse issue-card mt-3 " + this.cardStyle(issue)}>
           <div className="card-block">
             <h4 className="card-title">{issue.key}</h4>
             <h6 className="card-subtitle">{issue.title}</h6>
-          </div>
-          <div className="card-footer text-muted">
-            {issue.type} {issue.state}
           </div>
         </div>
       </div>
