7de65444aaecab5685a89b2462269b1e5a4ddd36
Dennis Schroer <dennis.schroer@topicus.nl>
2017-06-09 16:58:02 +0200

    Improve styling

--- ui/src/components/IssueList/Issue.js
+++ ui/src/components/IssueList/Issue.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+import './Issue.css';
+
 class Issue extends Component {
   cardStyle() {
     let status = this.props.status;
